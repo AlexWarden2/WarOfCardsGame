1766f5793bd179635a9695bc75831c005d027fe5
sherine <sherinebollarapu@gmail.com>
2023-11-02 18:33:41 +0000

    add cards array, CPU variable that randonly chooses a number from the cards array and Player1 variable that also randomly chooses from cards

--- numberGen.js
+++ numberGen.js
@@ -1,17 +1,17 @@
 // Writting the code to generate a random number for CPU and Player1 
 
-const cardsValue = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+const cards = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11, 12, 12, 12, 12, 13, 13, 13, 13, 14, 14]
 
-// function cardForCPU() {
-//     let CPU = cardsValue[Math.floor(Math.random() * cardsValue.length)]
+// sorry I made the card array so long, but this was the only thing I could think of to mimic the probability of an actual deck, 
+//as when I tried it before I got 14 3 times in a row!
 
-// }
+let CPU = cards[Math.floor(Math.random() * cards.length)] // this works and Alex can use this variable as the card value... how do i make it so that number 14 shows up less than others?
 
-let CPU = cardsValue[Math.floor(Math.random() * cardsValue.length)] // this works and Alex can use this variable as the card value... how do i make it so that number 14 shows up less than others?
+let Player1 = cards[Math.floor(Math.random() * cards.length)] // 
 
-let Player1 = cardsValue[Math.floor(Math.random() * cardsValue.length)] // 
-
-console.log(CPU)
-console.log(Player1)
+// console.log(CPU)
+// console.log(Player1)
+ 
+console.log(cards.length)
 
 
