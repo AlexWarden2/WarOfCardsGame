f9cc242e937dc8bb71a80819fc8d8b47516f0306
Alex Warden <alex.wardenn@gmail.com>
2023-11-03 11:46:05 +0000

    add packages and .gitignore

---
+++ game.js
@@ -0,0 +1,17 @@
+/* 
+Will take 2 arguments each round:
+ - CPU
+ - Player1
+
+ These will both be numbers - but should have interface for tarnslating them into picture cards where relevant 
+
+ will need a y/n to keep the loop going for eeach round to stop it immediatley finishing
+ 
+ must be merged by 1pm tomorrow
+
+ would be insanely hard version where it keeps track of which card from each array on each players hand - I dont think this was the idea when game suggested as medium 
+
+ may be possible - will need array of 4 of each number and two jokers 
+ will then need to keep track of each array and a serperate play array that gets added to winners hand each time 
+
+*/ 
